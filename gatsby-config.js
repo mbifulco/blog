005bc591630e2f8b0b30081bd7552502a1edfd04
Mike Bifulco <mbifulco@live.com>
2019-02-25 13:50:25 -0500

    replace GA with GTM
    but still use GA for outbound links

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,27 +116,11 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-google-tagmanager',
       options: {
-        trackingId: 'UA-3104942-19',
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'mike.biful.co',
+        id: 'GTM-PZRPQ5H',
+        // set this to true to make GTM work in dev environment (for testing/debug)
+        includeInDevelopment: false,
       },
     },
   ],
