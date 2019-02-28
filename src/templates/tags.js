64c1387170531d77c8bdb8ed45d53588debe98dc
Mike Bifulco <mbifulco@live.com>
2019-02-28 08:11:22 -0500

    add page templates for tags

---
+++ src/templates/tags.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Link, graphql } from 'gatsby'
+
+import Pluralize from 'pluralize'
+
+const Tags = ({ pageContext, data }) => {
+  const { tag } = pageContext
+  const { edges, totalCount } = data.allMarkdownRemark
+
+  const tagHeader = `${totalCount} ${Pluralize(
+    'post',
+    totalCount
+  )} tagged with "${tag}"`
+
+  return (
+    <div>
+      <h1>{tagHeader}</h1>
+      <ul>
+        {edges.map(({ node }) => {
+          const { path, title } = node.frontmatter
+          return (
+            <li key={path}>
+              <Link to={path}>{title}</Link>
+            </li>
+          )
+        })}
+      </ul>
+
+      <Link to="/tags">All tags</Link>
+    </div>
+  )
+}
+
+Tags.propTypes = {
+  pageContext: PropTypes.shape({
+    tag: PropTypes.string.isRequired,
+  }),
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      totalCount: PropTypes.number.isRequired,
+      edges: PropTypes.arrayOf(
+        PropTypes.shape({
+          node: PropTypes.shape({
+            frontmatter: PropTypes.shape({
+              path: PropTypes.string.isRequired,
+              title: PropTypes.string.isRequired,
+            }),
+          }),
+        }).isRequired
+      ),
+    }),
+  }),
+}
+
+export default Tags
+
+export const pageQuery = graphql`
+  query($tag: String) {
+    allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+          }
+        }
+      }
+    }
+  }
+`
