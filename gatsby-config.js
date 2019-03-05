6ace7aaf3f47ff496324544a1d6f15bfcbf5eee8
Mike Bifulco <mbifulco@live.com>
2019-03-04 20:00:14 -0500

    remove future-unused bits from config

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,27 +34,6 @@ module.exports = {
     `babel-preset-gatsby`,
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/src/posts`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-postcss`,
       options: {
         postCssPlugins: [
