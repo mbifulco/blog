5d4255fdfbe5dd9d185174c46c99e390d1085b70
Mike Bifulco <mbifulco@live.com>
2019-03-19 21:26:24 -0400

    add a sold out component

---
+++ src/components/soldOut.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from '../styles/soldout.module.css'
+
+const soldOut = () => (
+  <figure className={classes.soldOut}>
+    <div>Sold Out!</div>
+  </figure>
+)
+
+export default soldOut
