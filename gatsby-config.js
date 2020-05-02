ea84aedf87a3248aee422921e559380683c877e9
Mike Bifulco <mbifulco@live.com>
2020-05-02 12:18:27 -0400

    add component for headshot

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,13 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
---
+++ src/components/Headshot/Headshot.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
+
+const Headshot = ({ className }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "mike-headshot-square.png" }) {
+        id
+        childImageSharp {
+          fixed(width: 50, height: 50) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Img
+      className={`headshot ${className}`}
+      fixed={data.file.childImageSharp.fixed}
+    />
+  )
+}
+
+export default Headshot
