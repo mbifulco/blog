6a2f61a634898fa754e31e2f1fe876d28bfd6413
Mike Bifulco <mbifulco@live.com>
2020-05-09 11:17:28 -0400

    newsletter page

---
+++ src/pages/newsletter/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../../components/layout';
+import { NewsletterSignup } from '../../components/NewsletterSignup';
+
+const NewsletterPage = () => {
+  return (
+    <Layout>
+      <NewsletterSignup />
+    </Layout>
+  );
+};
+
+export default NewsletterPage;
