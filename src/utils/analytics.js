dabbb3f3b3ddc54a518086a315dc1741a31e84c3
Mike Bifulco <mbifulco@live.com>
2020-06-01 15:12:48 -0400

    bug fix

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,20 +1,11 @@
 /* global fathom */
 
-import React, { createContext, useContext, useEffect } from 'react';
+import React, { createContext, useContext } from 'react';
 import PropTypes from 'prop-types';
 
 const AnalyticsContext = createContext({});
 
 export const AnalyticsProvider = ({ children }) => {
-  useEffect(() => {
-    if (typeof fathom === 'undefined') {
-      fathom = (x, y, z) => {
-        // eslint-disable-next-line no-console
-        console.log(`I'm a fake Fathom`, x, y, z);
-      };
-    }
-  }, []);
-
   const logClicks = (goalId, valueInCents = 0) => {
     if (fathom && fathom.trackGoal) {
       fathom.trackGoal(goalId, valueInCents);
