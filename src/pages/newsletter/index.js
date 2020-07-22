a6d9407a368521bed068d3405779b4c67fcfac79
Mike Bifulco <mbifulco@live.com>
2020-07-22 19:47:38 -0400

    small fixes

--- src/pages/newsletter/index.js
+++ src/pages/newsletter/index.js
@@ -5,14 +5,12 @@ import { Heading } from '@chakra-ui/core';
 import { DefaultLayout as Layout } from '../../components/Layouts';
 import { NewsletterSignup } from '../../components/NewsletterSignup';
 
-import classes from './NewsletterPage.module.css';
-
 const NewsletterPage = () => {
   return (
     <Layout>
-      <div className={classes.container}>
-        <Heading as="h1">Join the newsletter</Heading>
-      </div>
+      <Heading as="h1" marginBottom="2rem">
+        Join the newsletter
+      </Heading>
       <NewsletterSignup />
     </Layout>
   );
