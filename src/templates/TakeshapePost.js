92816ea2f9fddd1f3b8531b7f5aeae85692f2d12
Mike Bifulco <mbifulco@live.com>
2020-09-18 13:25:14 -0400

    ts routing again

--- src/templates/TakeshapePost.js
+++ src/templates/TakeshapePost.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
-import { getImageUrl } from 'takeshape-routing';
+import { getImageUrl } from '@takeshape/routing';
 
 import SEO from '../components/seo';
 import { DefaultLayout as Layout } from '../components/Layouts';
