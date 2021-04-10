132af173a8a4a457a5f7a4ed761c105a98afc87d
Mike Bifulco <mbifulco@live.com>
2021-04-10 17:09:57 -0400

    remove unused stuff

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,5 @@
-/* eslint-disable react/prop-types */
-const LogRocket = require('logrocket');
-
 // prismjs color theme
 require('./src/styles/prism.css');
-
-// start up logrocket
-LogRocket.init('mike-bifulco/mikebifulco');
 
 const React = require('react');
 const { AnalyticsProvider } = require('./src/utils/analytics');
