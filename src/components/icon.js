6a713fd218bb00f2e4d2e2de95d9dbd3538e6442
Mike Bifulco <mbifulco@live.com>
2021-10-02 09:53:58 -0400

    remove css module

--- src/components/icon.js
+++ src/components/icon.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Flex, Text } from '@chakra-ui/react'
+
 import * as style from '../styles/icon.module.scss';
 
 const Icon = (props) => {
   const { d, size = '1em', label, style: styles } = props;
 
   return (
-    <span className={style.root} style={styles} role="figure">
+    <Flex
+      display="inline-flex"
+      alignItems="center"
+      justifyContent="center"
+      style={styles}
+      role="figure"
+    >
       <svg
         version="1.1"
         width={size}
@@ -15,10 +23,19 @@ const Icon = (props) => {
         viewBox="0 0 48 48"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path d={d} className={style.icon} />
+        <path d={d} className={style.icon} fill="currentColor" />
       </svg>
-      {label && <span className={style.label}>{label}</span>}
-    </span>
+      {label && (
+        <Text
+          position="relative"
+          display="inline-block"
+          margin-left="4px"
+          line-height={1}
+        >
+          {label}
+        </Text>
+      )}
+    </Flex>
   );
 };
 
