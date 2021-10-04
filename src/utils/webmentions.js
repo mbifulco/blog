8930c8095b29cac7a202df2b261da34e9d646262
Mike Bifulco <mbifulco@live.com>
2021-10-04 09:54:11 -0400

    remove console logs

--- src/utils/webmentions.js
+++ src/utils/webmentions.js
@@ -1,12 +1,10 @@
 export const getMentions = async (
   slug,
 ) => {
-  console.log(slug)
   const resp = await fetch(
     `https://webmention.io/api/mentions.json?target=https://mikebifulco.com${slug}`
   );
   const list = await resp.json();
 
-  console.log(list.links)
   return list.links;
 };
