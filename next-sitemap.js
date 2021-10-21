906d94750dd49bed65f77305ad80cae6baa81f31
Mike Bifulco <mbifulco@live.com>
2021-10-21 11:10:20 -0400

    robots.txt true, netlify always sets build env to prod

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://mikebifulco.com',
-  generateRobotsTxt: process.env.NODE_ENV === 'production',
+  generateRobotsTxt: true,
   // ...other options
 };
