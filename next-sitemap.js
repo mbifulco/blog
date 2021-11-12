ebd6efdee566861b1c4d823be912678b1792779e
Mike Bifulco <mbifulco@live.com>
2021-11-12 12:07:43 -0500

    robots.txt in prod only

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://mikebifulco.com',
-  generateRobotsTxt: true,
+  generateRobotsTxt: process.env.CONTEXT === 'production', // only generate robots.txt for prod
   // ...other options
 };
