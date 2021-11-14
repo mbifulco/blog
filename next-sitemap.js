fe12c0b58a2737a0a18972f79a767f5aafe50d27
Mike Bifulco <mbifulco@live.com>
2021-11-14 09:41:34 -0500

    no longer using next-sitemap

--- next-sitemap.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://mikebifulco.com',
-  generateRobotsTxt: process.env.CONTEXT === 'production', // only generate robots.txt for prod
-  // ...other options
-};
