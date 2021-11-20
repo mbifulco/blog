6edd42baecfbede6638fb7151439b3279e8fca01
Mike Bifulco <mbifulco@live.com>
2021-11-20 10:03:45 -0500

    fix 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,50 @@
 import React from 'react';
 
+import { Heading, Link, Text, useTheme } from '@chakra-ui/react';
+
 import { DefaultLayout } from '../components/Layouts';
-import SEO from '../components/seo';
+import { Image, SEO } from '../components';
 
-const NotFoundPage = () => (
-  <DefaultLayout>
-    <SEO title="404: Not found" />
-    <h1>{"Hey, shit, sorry, this -- isn't a page."}</h1>
-    <p>{"If you're expecting something to be here, tweet at me angrily."}</p>
-  </DefaultLayout>
-);
+const NotFoundPage = () => {
+  const theme = useTheme();
+  const pink = theme.colors.pink[600];
 
+  return (
+    <DefaultLayout>
+      <SEO title="404: URL Not found" />
+      <Heading as="h1">Uh oh, you stumbled upon a bad URL.</Heading>
+      <Image
+        publicId="pawel-czerwinski-lWBZ01XRRoI-unsplash_fyoisl"
+        alt="A soothing potted plant to atone for a missing page"
+        caption={
+          <span>
+            Photo by{' '}
+            <Link
+              color={pink}
+              href="https://unsplash.com/@pawel_czerwinski?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            >
+              Pawel Czerwinski
+            </Link>{' '}
+            on{' '}
+            <Link
+              color={pink}
+              href="https://unsplash.com/s/photos/plant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            >
+              Unsplash
+            </Link>
+          </span>
+        }
+      />
+      <Text as="p">
+        Hey, shit, sorry, this &mdash; {"isn't"} a page. If {"you're "}
+        expecting something to be here,{' '}
+        <Link color={pink} href="https://twitter.com/irreverentmike">
+          tweet at me (@irreverentmike)
+        </Link>{' '}
+        and {"I'll"} do my best to help you out..
+      </Text>
+    </DefaultLayout>
+  );
+};
 
 export default NotFoundPage;
