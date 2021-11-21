ab1cfe671c0bcc561b2d34a21382a3fe8e9dbbf9
Mike Bifulco <mbifulco@live.com>
2021-11-21 15:32:35 -0500

    rekajigger footer - related content topics by tag

---
+++ src/components/RelatedContent/RelatedContentLinksByTag.js
@@ -0,0 +1,48 @@
+import { Box, Link, SimpleGrid, Text, useTheme } from '@chakra-ui/react';
+import { NextLink } from 'next/link';
+
+import TagDictionary, { getTagInformation } from '../../data/ConvertKitTags';
+
+const DEFAULT_TAGS_TO_DISPLAY = [
+  'developer',
+  'javascript',
+  'design',
+  'css',
+  'ux',
+  'tools',
+  'productivity',
+  'gatsby',
+];
+
+const RelatedContentLinksByTag = ({ tags = DEFAULT_TAGS_TO_DISPLAY }) => {
+  const theme = useTheme();
+
+  const pink = theme.colors.pink[600];
+
+  return (
+    <Box>
+      <Text fontWeight="bold">More great resources</Text>
+      <SimpleGrid fontSize="large" minChildWidth="50%" spacingY="0.5rem">
+        {tags.map((tag) => {
+          const tagInformation = getTagInformation(tag);
+          if (!tagInformation) return null;
+          return (
+            <Link
+              textDecoration="underline"
+              _hover={{
+                textDecoration: 'none',
+              }}
+              as={NextLink}
+              href={`/tags/${tag}`}
+              key={`related-content-${tag}`}
+            >
+              Articles {tagInformation.label}
+            </Link>
+          );
+        })}
+      </SimpleGrid>
+    </Box>
+  );
+};
+
+export default RelatedContentLinksByTag;
--- src/data/ConvertKitTags.js
+++ src/data/ConvertKitTags.js
@@ -2,43 +2,55 @@ const tags = [
   {
     name: 'developer',
     id: '1456077',
+    label: 'for developers',
   },
   {
     name: 'gatsby',
     id: '1456078',
+    label: 'about Gatsby',
   },
   {
     name: 'css',
     id: '1456082',
+    label: 'about CSS',
   },
   {
     name: 'ux',
     id: '1456087',
+    label: 'about User Experience (UX)',
   },
   {
     name: 'designer',
     id: '1456088',
+    label: 'for designers',
   },
   {
     name: 'productivity',
     id: '1456091',
+    label: 'about productivity',
   },
   {
     name: 'tools',
     id: '1456093',
+    label: 'about tools I use',
   },
   {
     name: 'cycling',
     id: '1456097',
+    label: 'about cycling',
   },
   {
     name: 'javascript',
     id: '1456096',
+    label: 'for JavaScript developers',
   },
   {
     name: 'mental health',
     id: '1456098',
+    label: 'mental health and mindfulness',
   },
 ];
+
+export const getTagInformation = (tag) => tags.find((t) => t.name === tag);
 
 export default tags;
