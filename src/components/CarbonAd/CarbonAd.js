3a5c1ea6efef4ee12c318f0c3eda89db321e0153
Mike Bifulco <mbifulco@live.com>
2021-12-03 15:55:37 -0500

    add a CarbonAd component

---
+++ src/components/CarbonAd/CarbonAd.js
@@ -0,0 +1,15 @@
+import Script from 'next/script';
+import { Box } from '@chakra-ui/react';
+
+const CarbonAd = () => {
+  return (
+    <script
+      async
+      type="text/javascript"
+      src="//cdn.carbonads.com/carbon.js?serve=CESI553Y&placement=mikebifulcocom"
+      id="_carbonads_js"
+    />
+  );
+};
+
+export default CarbonAd;
---
+++ src/components/CarbonAd/index.js
@@ -0,0 +1,1 @@
+export { default as CarbonAd } from './CarbonAd';
