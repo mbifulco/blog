f998f9518acbadb768fbb06c2589d70941a705bf
Mike Bifulco <mbifulco@live.com>
2022-06-05 13:16:03 -0400

    cloudinary render bug hack

--- src/utils/images.js
+++ src/utils/images.js
@@ -8,7 +8,17 @@ export const getCloudinaryImageUrl = (publicId) => {
   });
 
   const myImage = cld.image(publicId).format('auto').quality('auto');
-  const url = myImage.toURL();
+  let url = myImage.toURL({ trackedAnalytics: false });
+
+  // HACK: This fixes a next.js issue with Cloudinary as of React 18, where server and client bundles were rendering differently
+  // Cloudinary seems to return images with a URL param of _a=[something] differently on each call of this function
+  // including between server and client - Next didn't like this
+  // related bug: https://github.com/vercel/next.js/discussions/35773
+  let x = new URL(url);
+  if (x.searchParams.has('_a')) {
+    x.searchParams.delete('_a');
+    url = x.toString();
+  }
 
   return url;
 };
