55db99f8025327c8809edcf3d29aa16fa6487b0d
Mike Bifulco <mbifulco@live.com>
2022-07-18 20:30:35 -0400

    clean up

--- src/components/MdxEmbedUtils/Vimeo.js
+++ src/components/MdxEmbedUtils/Vimeo.js
@@ -2,17 +2,15 @@ import { Box } from '@chakra-ui/react';
 
 const Vimeo = ({ vimeoId }) => {
   return (
-    <>
-      <Box
-        as="iframe"
-        src={`https://player.vimeo.com/video/${vimeoId}`}
-        width="640"
-        height="360"
-        frameBorder="0"
-        allow="autoplay; fullscreen; picture-in-picture"
-        allowFullScreen
-      />
-    </>
+    <Box
+      as="iframe"
+      src={`https://player.vimeo.com/video/${vimeoId}`}
+      width="640"
+      height="360"
+      frameBorder="0"
+      allow="autoplay; fullscreen; picture-in-picture"
+      allowFullScreen
+    />
   );
 };
 
