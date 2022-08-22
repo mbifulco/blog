23fe79b676b76356b0a7d44fa7164a81eb1446db
Mike Bifulco <mbifulco@live.com>
2022-08-22 07:34:39 -0600

    add html lang

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head />
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
