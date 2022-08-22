4f549248471f565fc4e701b0f6e7f9d67d163209
Mike Bifulco <mbifulco@live.com>
2022-08-21 18:55:25 -0600

    use next/script for ad tag

--- src/components/CarbonAd/CarbonAd.js
+++ src/components/CarbonAd/CarbonAd.js
@@ -1,6 +1,7 @@
+import Script from 'next/script';
 const CarbonAd = () => {
   return (
-    <script
+    <Script
       async
       type="text/javascript"
       src="//cdn.carbonads.com/carbon.js?serve=CESI553Y&placement=mikebifulcocom"
