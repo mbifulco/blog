9a696e3a2549274b38c86c0f6ffab8dbdaa4749a
Mike Bifulco <mbifulco@live.com>
2022-09-06 14:21:10 +0000

    add newsletter backissues

--- src/pages/newsletter/index.js
+++ src/pages/newsletter/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 
-import { Heading } from '@chakra-ui/react';
+import Link from 'next/link';
+
+import { Heading, Text } from '@chakra-ui/react';
 
 import { SEO } from '../../components';
 import { DefaultLayout as Layout } from '../../components/Layouts';
 import { NewsletterSignup } from '../../components/NewsletterSignup';
 
-const NewsletterPage = () => (
+import { getAllNewsletters } from '../../lib/newsletters';
+
+export async function getStaticProps() {
+  const newsletters = await getAllNewsletters();
+
+  return {
+    props: {
+      newsletters,
+    },
+  };
+}
+
+const NewsletterPage = ({ newsletters }) => (
   <Layout>
-    <SEO title="Join the newsletter: articles software dev, design, and climate" />
+    <SEO title="Subscribe to Tiny Improvements: articles software dev, design, and climate" />
     <Heading as="h1" marginBottom="2rem">
-      Join the newsletter
+      Subscribe to Tiny Improvements
     </Heading>
+    <Text>
+      An occasional newsletter by me, for you. You&apos;ll get my thoughts on
+      design and development, building products and companies, and philosophy
+      for living a life you love in an ever-changing world.
+    </Text>
+
+    {newsletters.map((newsletter) => {
+      const { slug, title } = newsletter?.frontmatter;
+      return (
+        <Heading as="h2" key={slug}>
+          <Link href={`/newsletter/${slug}`}>{title}</Link>
+        </Heading>
+      );
+    })}
+
     <NewsletterSignup />
   </Layout>
 );
