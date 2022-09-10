6eefe8ec4551c03d73b638e1c459648250b7f240
Mike Bifulco <mbifulco@live.com>
2022-09-10 10:13:23 -0400

    fix layout for image

--- src/components/NewsletterFeed/NewsletterItem.js
+++ src/components/NewsletterFeed/NewsletterItem.js
@@ -1,33 +1,26 @@
 import Link from 'next/link';
-import {
-  Box,
-  Center,
-  Heading,
-  Text,
-  Stack,
-  Avatar,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { Box, Heading, Text, Stack, useColorModeValue } from '@chakra-ui/react';
 
 import { Image } from '..';
 import TagsSummary from '../tagsSummary';
 import formatDate from '../../utils/format-date';
+import { getCloudinaryImageUrl } from '../../utils/images';
 
 const NewsletterItem = ({ newsletter, as = 'h3' }) => {
   const { coverImagePublicId, date, excerpt, slug, tags, title } =
     newsletter.frontmatter;
 
   return (
-    <Center py={6}>
-      <Box
-        maxW={'445px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.900')}
-        boxShadow={'2xl'}
-        rounded={'md'}
-        p={6}
-        overflow={'hidden'}
-      >
+    <Box
+      maxW={'445px'}
+      w={'full'}
+      bg={useColorModeValue('white', 'gray.900')}
+      boxShadow={'2xl'}
+      rounded={'md'}
+      p={6}
+      overflow={'hidden'}
+    >
+      <Link href={`/newsletter/${slug}`}>
         <Box
           h={'210px'}
           bg={'gray.100'}
@@ -35,47 +28,46 @@ const NewsletterItem = ({ newsletter, as = 'h3' }) => {
           mx={-6}
           mb={6}
           pos={'relative'}
+          backgroundImage={getCloudinaryImageUrl(coverImagePublicId)}
+          backgroundSize="cover"
+        />
+      </Link>
+      <Stack>
+        <Text
+          color={'pink.500'}
+          textTransform={'uppercase'}
+          fontWeight={800}
+          fontSize={'sm'}
         >
-          <Link href={`/newsletter/${slug}`}>
-            <Image
-              publicId={coverImagePublicId}
-              alt={`Cover image for newsletter ${title}`}
-              maxHeight="210px"
-              height="210px"
-              width="400px"
-              layout="fill"
-            />
+          📰 {formatDate(date)}
+        </Text>
+        <Heading
+          color={useColorModeValue('gray.700', 'white')}
+          fontSize={'xl'}
+          fontFamily={'body'}
+          as={as}
+          height="2.5em"
+        >
+          <Link href={`/newsletter/${slug}`}>{title}</Link>
+        </Heading>
+        <Text
+          color={'gray.500'}
+          height="6rem"
+          overflowY={'hidden'}
+          textOverflow="ellipsis"
+        >
+          {excerpt}
+        </Text>
+      </Stack>
+      <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
+        <Stack direction={'column'} spacing={0} fontSize={'sm'}>
+          <TagsSummary tags={tags} />
+          <Link href={`/newsletter/${slug}`} color={'gray.600'}>
+            6min read
           </Link>
-        </Box>
-        <Stack>
-          <Text
-            color={'pink.500'}
-            textTransform={'uppercase'}
-            fontWeight={800}
-            fontSize={'sm'}
-          >
-            📰 {formatDate(date)}
-          </Text>
-          <Heading
-            color={useColorModeValue('gray.700', 'white')}
-            fontSize={'xl'}
-            fontFamily={'body'}
-            as={as}
-          >
-            <Link href={`/newsletter/${slug}`}>{title}</Link>
-          </Heading>
-          <Text color={'gray.500'}>{excerpt}</Text>
         </Stack>
-        <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
-          <Stack direction={'column'} spacing={0} fontSize={'sm'}>
-            <TagsSummary tags={tags} />
-            <Link href={`/newsletter/${slug}`} color={'gray.600'}>
-              6min read
-            </Link>
-          </Stack>
-        </Stack>
-      </Box>
-    </Center>
+      </Stack>
+    </Box>
   );
 };
 
