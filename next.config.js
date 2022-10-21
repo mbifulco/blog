955392f371464a5e31dec7de260e82cebb097038
Mike Bifulco <mbifulco@live.com>
2022-10-20 21:32:15 -0400

    update config

--- next.config.js
+++ next.config.js
@@ -1,49 +1,35 @@
-// next.config.js
-const withMDX = require('@next/mdx')({
-  extension: /\.(md|mdx)$/,
-  options: {
-    providerImportSource: '@mdx-js/react',
-    remarkPlugins: [],
-    rehypePlugins: [],
+const withMarkdoc = require('@markdoc/next.js');
+
+module.exports = withMarkdoc({ schemaPath: './src/markdoc/' })({
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  images: {
+    domains: ['i.ytimg.com', 'res.cloudinary.com', 'images.unsplash.com'],
+  },
+  productionBrowserSourceMaps: true,
+  compress: false,
+  webpack: (config, context) => {
+    return {
+      ...config,
+      optimization: {
+        ...config?.optimzation,
+        minimize: false,
+      },
+    };
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/meet',
+        destination:
+          'https://calendar.google.com/calendar/u/0/appointments/AcZssZ0QlIIDE1tJhxGyqfaTa-ap5wT9tKqrA7PpxWs=',
+        permanent: false,
+      },
+      {
+        source: '/meet/pod',
+        destination:
+          'https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ2Zi9tQAqlVuP3eLMftB7i7JOBoKdfMxB_GKNJHeE-CQJS6q8YMCUa1KJRJaZ4NprJ516JUAtYP',
+        permanent: false,
+      },
+    ];
   },
 });
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-module.exports = withBundleAnalyzer(
-  withMDX({
-    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-    images: {
-      domains: ['i.ytimg.com', 'res.cloudinary.com', 'images.unsplash.com'],
-    },
-    productionBrowserSourceMaps: true,
-    compress: false,
-    webpack: (config, context) => {
-      return {
-        ...config,
-        optimization: {
-          ...config?.optimzation,
-          minimize: false,
-        },
-      };
-    },
-    async redirects() {
-      return [
-        {
-          source: '/meet',
-          destination:
-            'https://calendar.google.com/calendar/u/0/appointments/AcZssZ0QlIIDE1tJhxGyqfaTa-ap5wT9tKqrA7PpxWs=',
-          permanent: false,
-        },
-        {
-          source: '/meet/pod',
-          destination:
-            'https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ2Zi9tQAqlVuP3eLMftB7i7JOBoKdfMxB_GKNJHeE-CQJS6q8YMCUa1KJRJaZ4NprJ516JUAtYP',
-          permanent: false,
-        },
-      ];
-    },
-  })
-);
