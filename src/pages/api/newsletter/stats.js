fd3d64cc9b062de060296062db840f2d3f26140c
Mike Bifulco <mbifulco@live.com>
2022-11-13 19:38:34 -0500

    sure don't want that console log in here

--- src/pages/api/newsletter/stats.js
+++ src/pages/api/newsletter/stats.js
@@ -12,8 +12,6 @@ export default async function handler(req, res) {
 
   const { stats } = json;
 
-  console.log('api got back stats', stats);
-
   res.status(200).json({
     subscriberCount: stats.total_subscribers,
   });
