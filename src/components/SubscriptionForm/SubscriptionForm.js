b089601e81eacecc9623dc18ac2058be2d18cdf7
Mike Bifulco <mbifulco@live.com>
2023-01-08 15:54:55 -0500

    simplify and make this form work

--- src/components/SubscriptionForm/SubscriptionForm.js
+++ src/components/SubscriptionForm/SubscriptionForm.js
@@ -16,6 +16,7 @@ const SubscriptionForm = ({ tags }) => {
         data-version="5"
         data-options='{"settings":{"after_subscribe":{"action":"message","success_message":"Success! Now check your email to confirm your subscription.","redirect_url":""},"analytics":{"google":null,"fathom":null,"facebook":null,"segment":null,"pinterest":null,"sparkloop":null,"googletagmanager":null},"modal":{"trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"powered_by":{"show":true,"url":"https://convertkit.com/features/forms?utm_campaign=poweredby&amp;utm_content=form&amp;utm_medium=referral&amp;utm_source=dynamic"},"recaptcha":{"enabled":false},"return_visitor":{"action":"custom_content","custom_content":"Thanks for subscribing!"},"slide_in":{"display_in":"bottom_right","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"sticky_bar":{"display_in":"top","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15}},"version":"5"}'
         min-width="400 500 600 700 800"
+        style={{ width: '100%' }}
       >
         <div data-style="clean">
           <ul
@@ -23,23 +24,17 @@ const SubscriptionForm = ({ tags }) => {
             data-element="errors"
             data-group="alert"
           ></ul>
-          <div
+          <Flex
             data-element="fields"
             data-stacked="false"
             className="seva-fields formkit-fields"
-            style={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              width: '100%',
-              maxWidth: '100%',
-              margin: '0px',
-              padding: '0px',
-              alignItems: 'center',
-              border: '1px solid #ED64A6',
-              borderRadius: '4px',
-            }}
+            flexDir={['column', 'column', 'row']}
+            width="100%"
+            alignItems={'center'}
+            border="1px solid #ED64A6"
+            borderRadius="4px"
           >
-            <div className="formkit-field">
+            <div className="formkit-field" style={{ flexGrow: 1 }}>
               <input
                 className="formkit-input"
                 aria-label="First Name"
@@ -49,8 +44,9 @@ const SubscriptionForm = ({ tags }) => {
                 type="text"
                 style={{
                   color: 'rgb(0, 0, 0)',
+                  borderRight: '1px solid',
                   borderColor: 'rgb(237, 100, 166)',
-                  borderRight: '1px solid #ED64A6',
+                  // borderBottom: '1px solid #ED64A6',
                   fontWeight: 400,
                   padding: '1ch 2ch',
                   borderRadius: '4px 0px 0px 4px',
@@ -58,7 +54,10 @@ const SubscriptionForm = ({ tags }) => {
                 }}
               />
             </div>
-            <div className="formkit-field" style={{ borderRadius: 0 }}>
+            <div
+              className="formkit-field"
+              style={{ borderRadius: 0, flexGrow: 2 }}
+            >
               <input
                 className="formkit-input"
                 name="email_address"
@@ -72,7 +71,7 @@ const SubscriptionForm = ({ tags }) => {
                   borderRadius: '0',
                   padding: '1ch 2ch',
                   fontWeight: 400,
-                  minWidth: '30ch',
+                  width: '100%',
                 }}
               />
             </div>
@@ -85,6 +84,7 @@ const SubscriptionForm = ({ tags }) => {
                 borderRadius: '0px',
                 padding: '1ch 8ch',
                 fontWeight: 400,
+                flexGrow: 1,
               }}
             >
               <div className="formkit-spinner">
@@ -94,7 +94,7 @@ const SubscriptionForm = ({ tags }) => {
               </div>
               <span className="">I&apos;m in!</span>
             </button>
-          </div>
+          </Flex>
         </div>
       </form>
     </Flex>
