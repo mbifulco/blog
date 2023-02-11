060d5a5b8144c936a3cdea38c80ac771c89dd4e3
Mike Bifulco <mbifulco@live.com>
2023-02-11 11:04:25 -0500

    filter my own mentions

--- src/components/Post/mentionsSummary.js
+++ src/components/Post/mentionsSummary.js
@@ -22,10 +22,21 @@ const MentionsSummary = ({ mentions }) => {
 
   if (!mentions || mentions.length === 0) return null;
 
+  console.log(mentions);
+
   const likes = mentions.filter((mention) => mention.activity.type === 'like');
-  const someoneMentioned = mentions.filter(
-    (mention) => mention.activity.type !== 'like'
-  );
+  const someoneMentioned = mentions.filter((mention) => {
+    if (mention.activity.type === 'like') return false;
+    if (
+      mention?.data?.author?.url === 'https://reddit.com/user/irreverentmike/'
+    )
+      return false;
+    if (mention?.data?.author?.url === 'https://hachyderm.io/@irreverentmike')
+      return false;
+    if (mention?.data?.author?.url === 'https://twitter.com/irreverentmike')
+      return false;
+    return true;
+  });
 
   const avatarSize = '48px';
 
