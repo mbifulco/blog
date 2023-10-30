b32f3e2882187690e1d611852a0fc652505a5300
Mike Bifulco <mbifulco@live.com>
2023-10-30 17:02:15 +0700

    chore: more ts

--- src/components/PublishDate/PublishDate.js
+++
@@ -1,8 +0,0 @@
-import { Text } from '@chakra-ui/react';
-import formatDate from '../../utils/format-date';
-
-const PublishDate = ({ date }) => {
-  return <time dateTime={date}>{formatDate(date, 'MMMM dd, yyyy')}</time>;
-};
-
-export default PublishDate;
