e74895c0e9268eb0726ee27a61809ae88e0bf8fc
Mike Bifulco <mbifulco@live.com>
2023-10-30 17:41:11 +0700

    chore: more ts

---
+++ src/components/Colophon/Colophon.tsx
@@ -0,0 +1,15 @@
+const Colophon = () => {
+  return (
+    <div className="grid grid-cols-3">
+      <span />
+      <div className="flex flex-row gap-4 justify-evenly text-6xl text-pink-600">
+        <span>*</span>
+        <span>*</span>
+        <span>*</span>
+      </div>
+      <span />
+    </div>
+  );
+};
+
+export default Colophon;
