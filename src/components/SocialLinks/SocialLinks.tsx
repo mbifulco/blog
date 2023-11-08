b9af840771ba14b9f4dade01c9f21723ca4f7645
Mike Bifulco <mbifulco@live.com>
2023-11-08 07:56:53 -0500

    chore: tweak social links & footer

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -4,13 +4,52 @@ import React from 'react';
 import Link from 'next/link';
 import { FaYoutube } from 'react-icons/fa';
 
-import { GitHubIcon, TwitterIcon, TwitchIcon, RssIcon } from '../icons';
+import {
+  GitHubIcon,
+  TwitterIcon,
+  TwitchIcon,
+  RssIcon,
+  YouTubeIcon,
+  ThreadsIcon,
+} from '../icons';
 import MastodonIcon from '../icons/MastodonIcon';
 
 const SocialLinks = () => (
-  <div className="flex flex-row gap-8 items-center">
+  <div className="flex flex-row gap-6 items-center">
     <Link
-      className="hover:text-pink-600 m-1 h-4 w-4"
+      className="hover:scale-125 transition-all duration-200 hover:text-pink-600 m-1 h-4 w-4"
+      href="https://threads.net/@irreverentmike"
+      target="_blank"
+      rel="noreferrer noopener me"
+      aria-label="@irreverentmike on threads"
+    >
+      <ThreadsIcon />
+    </Link>
+
+    <Link
+      className="hover:scale-125 transition-all duration-200 hover:text-red m-1 h-4 w-4"
+      href="https://www.youtube.com/c/MikeBifulco"
+      target="_blank"
+      rel="noopener noreferrer me"
+      aria-label="MikeBifulco on youtube"
+    >
+      <span className="text-sm">
+        <YouTubeIcon />
+      </span>
+    </Link>
+
+    <Link
+      className="hover:scale-125 transition-all duration-200 hover:text-pink-600 m-1 h-4 w-4"
+      href="https://github.com/mbifulco"
+      target="_blank"
+      rel="noreferrer noopener me"
+      aria-label="@mbifulco on github"
+    >
+      <GitHubIcon />
+    </Link>
+
+    <Link
+      className="hover:scale-125 transition-all duration-200 hover:text-pink-600 m-1 h-4 w-4"
       target="_blank"
       rel="noopener noreferrer me"
       aria-label="@irreverentmike on Mastodon"
@@ -20,7 +59,7 @@ const SocialLinks = () => (
     </Link>
 
     <Link
-      className="hover:text-pink-600 m-1 h-4 w-4"
+      className="hover:scale-125 transition-all duration-200 hover:text-pink-600 m-1 h-4 w-4 hidden"
       href="https://twitter.com/irreverentmike"
       target="_blank"
       rel="noreferrer noopener me"
@@ -28,17 +67,9 @@ const SocialLinks = () => (
     >
       <TwitterIcon />
     </Link>
+
     <Link
-      className="hover:text-pink-600 m-1 h-4 w-4"
-      href="https://github.com/mbifulco"
-      target="_blank"
-      rel="noreferrer noopener me"
-      aria-label="@mbifulco on github"
-    >
-      <GitHubIcon />
-    </Link>
-    <Link
-      className="hover:text-pink-600 m-1 h-4 w-4"
+      className="hover:scale-125 transition-all duration-200 hover:text-pink-600 m-1 h-4 w-4 hidden"
       href="https://www.twitch.tv/irreverentmike"
       target="_blank"
       rel="noreferrer noopener"
@@ -46,19 +77,9 @@ const SocialLinks = () => (
     >
       <TwitchIcon />
     </Link>
+
     <Link
-      className="hover:text-pink-600 m-1 h-4 w-4"
-      href="https://www.youtube.com/c/MikeBifulco"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="MikeBifulco on youtube"
-    >
-      <span className="text-sm">
-        <FaYoutube />
-      </span>
-    </Link>
-    <Link
-      className="hover:text-pink-600 m-1 h-4 w-4"
+      className="hover:scale-125 transition-all duration-200 hover:text-pink-600 m-1 h-4 w-4"
       href="/rss.xml"
       target="_blank"
       rel="noopener noreferrer"
