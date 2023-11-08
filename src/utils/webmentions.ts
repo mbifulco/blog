f69131dbef717cef5598ebd9b1684c553409a08c
Mike Bifulco <mbifulco@live.com>
2023-11-08 08:28:06 -0500

    chore: configure eslint and start slaying

--- src/utils/webmentions.ts
+++ src/utils/webmentions.ts
@@ -1,8 +1,36 @@
+export type WebMention = {
+  source: string;
+  verified: boolean;
+  verified_date: string;
+  id: number;
+  private: boolean;
+  data: {
+    author: {
+      name: string;
+      url: string;
+      photo: string;
+    };
+    url: string;
+    name: string;
+    content: string;
+    published: string;
+    published_ts: number;
+  };
+  activity: {
+    type: string;
+    sentence_html: string;
+    sentence_text: string;
+  };
+  target: string;
+};
+
 export const getMentions = async (slug: string) => {
   const resp = await fetch(
     `https://webmention.io/api/mentions.json?target=https://mikebifulco.com${slug}`
   );
-  const list = await resp.json();
+  const list = (await resp.json()) as { links: WebMention[] };
 
   return list.links;
 };
+
+export default getMentions;
