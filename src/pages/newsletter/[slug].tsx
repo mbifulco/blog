a902c0f418a122ae63bfeeaea98649d6475fe84e
Mike Bifulco <mbifulco@live.com>
2023-11-30 20:29:30 -0500

    fix: fix

--- src/pages/newsletter/[slug].tsx
+++ src/pages/newsletter/[slug].tsx
@@ -75,7 +75,10 @@ const NewsletterPage: React.FC<NewsletterPageProps> = ({ newsletter }) => {
           image={coverImageUrl}
           ogType="article"
         />
-        <div className="max-w-4xl">
+        <div
+          className="w-[41 rem]
+          mx-auto max-w-4xl"
+        >
           <BreadCrumbs
             crumbs={[
               {
