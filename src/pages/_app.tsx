cf8ae97ae4317d5e02408181d1b07da0fa6f6da0
Mike Bifulco <mbifulco@live.com>
2023-12-13 20:11:30 -0500

    fix: same-same, but different

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,9 +18,9 @@ import DefaultLayout from '../components/Layouts/DefaultLayout';
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host:
-      process.env.NODE_ENV === 'development'
-        ? 'https://app.posthog.com'
-        : 'https://mikebifulco.com/ingest',
+      process.env.NODE_ENV === 'production'
+        ? 'https://mikebifulco.com/ingest'
+        : 'https://app.posthog.com',
     // Enable debug mode in development
     loaded: (posthog) => {
       if (process.env.NODE_ENV === 'development') posthog.debug();
