da7547ffa1bef598d5969c494534558052054139
Mike Bifulco <mbifulco@live.com>
2024-07-26 08:22:08 -0400

    fix: hide overflow

--- src/components/demos/OrtonEffectImage.tsx
+++ src/components/demos/OrtonEffectImage.tsx
@@ -22,58 +22,60 @@ export const OrtonEffectImage: React.FC<OrtonEffectImageProps> = ({
   const [blurRadius, setRedBlurRadius] = useState<number>(blurStart ?? 15);
 
   return (
-    <div className="flex flex-col gap-2">
-      <figure>
-        <img
-          src={src}
-          alt={alt}
-          style={{
-            mixBlendMode: 'lighten',
-            filter: `blur(${blurRadius}px)`,
-            opacity: `${opacity}%`,
-            position: 'absolute',
-          }}
-        />
-        <img src={src} aria-hidden={true} />
-      </figure>
+    <div className="flex max-w-full flex-col gap-2">
+      <div className="relative">
+        <figure className="relative overflow-hidden">
+          <img
+            src={src}
+            alt={alt}
+            style={{
+              mixBlendMode: 'lighten',
+              filter: `blur(${blurRadius}px)`,
+              opacity: `${opacity}%`,
+              position: 'absolute',
+            }}
+          />
+          <img src={src} aria-hidden={true} />
+        </figure>
 
-      {showControls && (
-        <div className="grid grid-cols-[12ch_1fr] items-center gap-2">
-          <p className="inline-block text-right font-mono text-sm uppercase">
-            Opacity:
-          </p>
-          <div className="center grow">
-            <LocalSlider
-              aria-label="Opacity Slider"
-              onChange={([val]) => setOpacity(val)}
-              minValue={5}
-              maxValue={100}
-              value={[opacity]}
-              numberFormatter={
-                { format: (val) => `${val}%` } as Intl.NumberFormat
-              }
-              label={'%'}
-            />
-          </div>
+        {showControls && (
+          <div className="grid grid-cols-[12ch_1fr] items-center gap-2">
+            <p className="inline-block text-right font-mono text-sm uppercase">
+              Opacity:
+            </p>
+            <div className="center grow">
+              <LocalSlider
+                aria-label="Opacity Slider"
+                onChange={([val]) => setOpacity(val)}
+                minValue={5}
+                maxValue={100}
+                value={[opacity]}
+                numberFormatter={
+                  { format: (val) => `${val}%` } as Intl.NumberFormat
+                }
+                label={'%'}
+              />
+            </div>
 
-          <p className="inline-block text-right font-mono text-sm uppercase">
-            Blur Radius:
-          </p>
-          <div className="grow">
-            <LocalSlider
-              aria-label="Blur Radius Slider"
-              onChange={([val]) => setRedBlurRadius(val)}
-              minValue={5}
-              maxValue={100}
-              value={[blurRadius]}
-              numberFormatter={
-                { format: (val) => `${val}px` } as Intl.NumberFormat
-              }
-              label={'px'}
-            />
+            <p className="inline-block text-right font-mono text-sm uppercase">
+              Blur Radius:
+            </p>
+            <div className="grow">
+              <LocalSlider
+                aria-label="Blur Radius Slider"
+                onChange={([val]) => setRedBlurRadius(val)}
+                minValue={5}
+                maxValue={100}
+                value={[blurRadius]}
+                numberFormatter={
+                  { format: (val) => `${val}px` } as Intl.NumberFormat
+                }
+                label={'px'}
+              />
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
