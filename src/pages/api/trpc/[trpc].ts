569ea30890435325296c93bfe681ed637fcb6867
Mike Bifulco <mbifulco@live.com>
2024-08-09 15:19:00 -0400

    chore: Update trpc API handler to use edge runtime for the cheapness

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,10 +1,17 @@
-import * as trpcNext from '@trpc/server/adapters/next';
+import { NextRequest } from 'next/server';
+import { appRouter } from '@server/routers/_app';
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 
-import { appRouter } from '../../../server/routers/_app';
+export default async function handler(req: NextRequest) {
+  return fetchRequestHandler({
+    endpoint: '/api/trpc',
+    router: appRouter,
+    req,
+    createContext: () => ({}),
+  });
+}
 
-// export API handler
-// @link https://trpc.io/docs/v11/server/adapters
-export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext: () => ({}),
-});
+// Use the edgeruntime
+export const config = {
+  runtime: 'edge',
+};
