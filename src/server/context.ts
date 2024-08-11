dbe40809d84f35c1638f3392877680b0d88575d0
Mike Bifulco <mbifulco@live.com>
2024-08-11 16:37:51 -0400

    fix: context types for non async

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,29 +1,32 @@
+// NOTE TO FUTURE SELF: there are handy /* */ comments
+// around the async / await stuff for when we need to add it back in
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type * as trpcNext from '@trpc/server/adapters/next';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface CreateContextOptions {
+type CreateContextOptions = {
   // session: Session | null
-}
+};
 
 /**
  * Inner function for `createContext` where we create the context.
  * This is useful for testing when we don't want to mock Next.js' request/response
  */
-export async function createContextInner(_opts: CreateContextOptions) {
+export /* async */ function createContextInner(_opts: CreateContextOptions) {
   return {};
 }
 
-export type Context = Awaited<ReturnType<typeof createContextInner>>;
+// export type Context = Awaited<ReturnType<typeof createContextInner>>;
+export type Context = ReturnType<typeof createContextInner>;
 
 /**
  * Creates context for an incoming request
  * @link https://trpc.io/docs/v11/context
  */
-export async function createContext(
+export /* async */ function createContext(
   opts: trpcNext.CreateNextContextOptions
-): Promise<Context> {
+): /* Promise<Context> */ Context {
   // for API-response caching see https://trpc.io/docs/v11/caching
 
-  return await createContextInner({});
+  return /* await */ createContextInner({});
 }
