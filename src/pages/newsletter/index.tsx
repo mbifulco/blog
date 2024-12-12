a9124154d275e4a9b820c42e9beba91d67ccbc5f
Mike Bifulco <mbifulco@live.com>
2024-12-12 07:45:36 -0500

    fix: make sure all newsletters have frontmatter

--- src/pages/newsletter/index.tsx
+++ src/pages/newsletter/index.tsx
@@ -73,6 +73,7 @@ const NewsletterPage: React.FC<NewsletterPageProps> = ({ newsletters }) => {
       </Heading>
       <div className="grid grid-cols-auto-fit-min-300 gap-5">
         {pastNewsletters.map((newsletter) => {
+          if (!newsletter || !newsletter.frontmatter) return null;
           const { slug } = newsletter.frontmatter;
           return <NewsletterItem newsletter={newsletter} key={slug} />;
         })}
