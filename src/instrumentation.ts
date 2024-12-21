0a00722f069578209c7aa49a1fa29bfd5be4b25e
Mike Bifulco <mbifulco@live.com>
2024-12-21 08:36:21 -0500

    fix?: surely vercel/otel wasn't to blame

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,5 @@
-import { registerOTel } from '@vercel/otel';
+// import { registerOTel } from '@vercel/otel';
 
-export function register() {
-  registerOTel({ serviceName: 'next-app' });
-}
+// export function register() {
+//   registerOTel({ serviceName: 'next-app' });
+// }
