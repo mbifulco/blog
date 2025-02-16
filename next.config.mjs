a7067a0b86f461e7e654f9ad045b1a3d5d28530f
Mike Bifulco <mbifulco@live.com>
2025-02-15 20:02:13 -0500

    chore: for now, don't minify

--- next.config.mjs
+++ next.config.mjs
@@ -9,73 +9,81 @@ jiti('./src/utils/env');
  * @type {import('next').NextConfig}
  **/
 const config = {
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.ytimg.com',
+  swcMinify: false,
+  webpack: (config, { dev, isServer }) => {
+    config.optimization.minimize = false;
+
+    return {
+      ...config,
+      pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+      images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'i.ytimg.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'res.cloudinary.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'images.unsplash.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'webmention.io',
+          },
+          {
+            protocol: 'https',
+            hostname: 'images.unsplash.com',
+          },
+        ],
       },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+      productionBrowserSourceMaps: true,
+      redirects: async () => {
+        return [
+          {
+            source: '/meet',
+            destination: 'https://savvycal.com/irreverentmike/30m',
+            permanent: false,
+          },
+          {
+            source: '/meet/pod',
+            destination: 'https://savvycal.com/irreverentmike/pod',
+            permanent: false,
+          },
+          {
+            source: '/sponsor',
+            destination: 'https://www.passionfroot.me/irreverentmike',
+            permanent: false,
+          },
+          {
+            source: '/tinyimprovements',
+            destination: '/newsletter',
+            permanent: false,
+          },
+        ];
       },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+      rewrites: async () => {
+        return [
+          {
+            source: '/ingest/static/:path*',
+            destination: 'https://us-assets.i.posthog.com/static/:path*',
+          },
+          {
+            source: '/ingest/:path*',
+            destination: 'https://us.i.posthog.com/:path*',
+          },
+          {
+            source: '/ingest/decide',
+            destination: 'https://us.i.posthog.com/decide',
+          },
+        ];
       },
-      {
-        protocol: 'https',
-        hostname: 'webmention.io',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
+      skipTrailingSlashRedirect: true,
+    };
   },
-  productionBrowserSourceMaps: true,
-  redirects: async () => {
-    return [
-      {
-        source: '/meet',
-        destination: 'https://savvycal.com/irreverentmike/30m',
-        permanent: false,
-      },
-      {
-        source: '/meet/pod',
-        destination: 'https://savvycal.com/irreverentmike/pod',
-        permanent: false,
-      },
-      {
-        source: '/sponsor',
-        destination: 'https://www.passionfroot.me/irreverentmike',
-        permanent: false,
-      },
-      {
-        source: '/tinyimprovements',
-        destination: '/newsletter',
-        permanent: false,
-      },
-    ];
-  },
-  rewrites: async () => {
-    return [
-      {
-        source: '/ingest/static/:path*',
-        destination: 'https://us-assets.i.posthog.com/static/:path*',
-      },
-      {
-        source: '/ingest/:path*',
-        destination: 'https://us.i.posthog.com/:path*',
-      },
-      {
-        source: '/ingest/decide',
-        destination: 'https://us.i.posthog.com/decide',
-      },
-    ];
-  },
-  skipTrailingSlashRedirect: true,
 };
 
 export default withBundleAnalyzer({ enabled: process.env.ANALYZE === true })(
