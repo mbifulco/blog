b23c7e00a265eee7f3ffca7223c743f5d8f69aa4
Mike Bifulco <mbifulco@live.com>
2025-07-15 09:43:49 -0400

    chore: more dev-savvy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,14 +26,19 @@ const fontVariables = [quickdraw.variable, dumpling.variable].join(' ');
 
 // Check that PostHog is client-side (used to handle Next.js SSR)
 if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: 'https://mikebifulco.com/ingest',
-    ui_host: 'https://app.posthog.com',
-    // Enable debug mode in development
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === 'development') posthog.debug();
-    },
-  });
+  if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host:
+        process.env.NODE_ENV === 'production'
+          ? 'https://mikebifulco.com/ingest'
+          : 'https://us.i.posthog.com',
+      ui_host: 'https://app.posthog.com',
+      // Enable debug mode in development
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug();
+      },
+    });
+  }
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
