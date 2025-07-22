56bc9be72f37c91a91b0426cbbb3c36a15a6f522
Mike Bifulco <mbifulco@live.com>
2025-07-22 11:32:44 -0400

    fix: input & label styles

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -9,7 +9,7 @@ const Label = React.forwardRef<
   <label
     ref={ref}
     className={clsxm(
-      "block text-sm font-medium text-zinc-500 dark:text-zinc-400",
+      "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
       className
     )}
     {...props}
