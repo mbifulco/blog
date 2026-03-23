5f280c26392ef102b2729a08b2f8aaff6d9e66bd
Mike Bifulco <mbifulco@live.com>
2026-03-23 14:01:07 -0400

    chore: normalize cn import paths and restore dialog animation duration

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -5,7 +5,7 @@ import { type DialogProps } from "@radix-ui/react-dialog"
 import { Command as CommandPrimitive } from "cmdk"
 import { Search } from "lucide-react"
 
-import { cn } from "@components/lib/utils"
+import { cn } from '@utils/cn'
 import { Dialog, DialogContent } from "@components/ui/dialog"
 
 const Command = React.forwardRef<
